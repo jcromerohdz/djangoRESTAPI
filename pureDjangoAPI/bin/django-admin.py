dac10b43aab64c22b4b3d82429cd46913e5f8ba1
Christian <jcromerohdz@gmail.com>
2019-04-03 17:59:37 -0700

    Set up initial djangoREST dev

---
+++ pureDjangoAPI/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chris/Documents/Proyectos/djangoRESTAPI/pureDjangoAPI/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
