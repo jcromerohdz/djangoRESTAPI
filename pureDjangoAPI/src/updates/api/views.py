f0075057dd170d050b2e1f6cd3acdfab377d7cb9
Christian <jcromerohdz@gmail.com>
2019-04-07 19:25:39 -0700

    Setup for manual api in pure django

---
+++ pureDjangoAPI/src/updates/api/views.py
@@ -0,0 +1,17 @@
+from updates.modls import Update as UpdateModel
+
+
+# Creating, Updating, Deleting, Retrieving (1) -- Update Model
+class UpdateModeAPI():
+    def  get(self, request, *args, **kwargs):
+
+        return #json httpresponse
+
+    def post(self, request, *args, **kwargs):
+        return #json
+
+    def put(self, request, *args, **kwargs):
+        return #json
+
+    def delete(self, request, *args, **kwargs):
+        return #json
