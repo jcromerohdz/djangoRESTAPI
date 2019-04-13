eb7e0c938098fa444be72f54e8c44c926c766fb1
Christian <jcromerohdz@gmail.com>
2019-04-13 10:45:56 -0700

    Implementing id functionality on the  put and delete within Update Model List APIView

--- pureDjangoAPI/src/updates/api/views.py
+++ pureDjangoAPI/src/updates/api/views.py
@@ -80,20 +80,51 @@ class UpdateModelDetailAPIView(HttpResponseMixin, CSRFExemptMixin, View):
         return self.render_to_response(json_data, status=200)
 
 
+# AUTH / Permissions -- DJANGO REST FRAMEWORK
+
+
 class UpdateModelListAPIView(HttpResponseMixin, CSRFExemptMixin, View):
     """
     List views
     Create view
+    Update
+    Delete
     """
     is_json = True
+    queryset = None
 
     # def render_to_response(data, status=200):
     #     return HttpResponse(data, content_type='application/json', status=status)
 
-    def get(self, request, *args, **kwargs):
+    def get_queryset(self):
         qs = UpdateModel.objects.all()
-        json_data = qs.serialize()
-        return self.render_to_response(json_data)
+        self.queryset = qs
+        return qs
+
+    def get_object(self, id=None):
+        # qs = UpdateModel.objects.filter(id=id)
+        if id is None:
+            return None
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
+    def get(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        passed_id = passed_data.get('id', None)
+        if passed_id is not None:
+            obj = self.get_object(id=passed_id)
+            if obj is None:
+                error_data = json.dumps("message": "Update not found")
+                return self.render_to_response(error_data, status=404)
+            json_data = obj.serialize()
+            return self.render_to_response(json_data)
+        else:
+            # qs = UpdateModel.objects.all()
+            qs = self.get_queryset()
+            json_data = qs.serialize()
+            return self.render_to_response(json_data)
 
     def post(self, request, *args, **kwargs):
         valid_json = is_json(request.body)
@@ -113,7 +144,69 @@ class UpdateModelListAPIView(HttpResponseMixin, CSRFExemptMixin, View):
         data  = json.dumps({"message":"Not Allowed"})
         return self.render_to_response(data, status=400)
 
+    # def delete(self, request, *args, **kwargs):
+    #     data  = json.dumps({"message": "You cannot delete an entire list."})
+    #     status_code = 403 # Not allowed
+    #     return self.render_to_response(data, status=status_code)
+
+    def put(self, request, *args, **kwargs):
+        valid_json = is_json(request.body)
+        if not valid_json:
+            error_data = json.dumps({"message": "Invalid data sent, please send using JSON."})
+            return self.render_to_response(error_data, status=400)
+
+        passed_data =json.loads(request.body)
+        passed_id = passed_data.get('id', None)
+
+        if not passed_id:
+            error_data = json.dumps({"message": "This is required field to update an item."})
+            return self.render_to_response(error_data, status=400)
+
+        obj = self.get_object(id=passed_id)
+        if obj is None:
+            error_data = json.dumps({"message": "Obeject not found"})
+            return self.render_to_response(error_data, status=404)
+        print(request.body)
+        # new_data = {}
+        data = json.loads(obj.serialize())
+
+        for key, value in passed_data.items():
+            data[key] = value
+
+        form = UpdateModelForm(data, instance=obj)
+        if form.is_valid():
+            obj = form.save(commit=True)
+            obj_data = json.dumps(data)
+            return self.render_to_response(obj_data, status=201)
+        if form.errors:
+            data  = json.dumps(form.errors)
+            return self.render_to_response(data, status=400)
+
+
+        # new_data = json.loads(request.body)
+        # print(new_data['content'])
+        json_data = json.dumps({"message": "Put functionality"})
+        return self.render_to_response(json_data)
+
     def delete(self, request, *args, **kwargs):
-        data  = json.dumps({"message": "You cannot delete an entire list."})
-        status_code = 403 # Not allowed
-        return self.render_to_response(data, status=status_code)
+        valid_json = is_json(request.body)
+        if not valid_json:
+            error_data = json.dumps({"message": "Invalid data sent, please send using JSON."})
+            return self.render_to_response(error_data, status=400)
+
+        passed_data =json.loads(request.body)
+        passed_id = passed_data.get('id', None)
+
+        if not passed_id:
+            error_data = json.dumps({"message": "This is required field to update an item."})
+            return self.render_to_response(error_data, status=400)
+
+        obj = self.get_object(id=passed_id)
+        if obj is None:
+            error_data = json.dumps({"message": "Obeject not found"})
+            return self.render_to_response(error_data, status=404)
+
+        deleted_=obj.delete()
+        print(deleted_)
+        json_data = json.dumps({"message": "Succesfully deleted."})
+        return self.render_to_response(json_data, status=200)
